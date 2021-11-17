c4d0529eadbdb711ab0b4c19d268a2278c544840
ailsons-dev <ailsons.dev@gmail.com>
2021-11-17 11:05:34 -0300

    Update test.py

--- test.py
+++ test.py
@@ -23,6 +23,6 @@ class Test(unittest.TestCase):
 
 
 if __name__ == "__main__":
-    print ('INICIANDO OS TESTES')
+    print ('INICIANDO OS NOVOS TESTES')
     print('----------------------------------------------------------------------')
     unittest.main(verbosity=2)
